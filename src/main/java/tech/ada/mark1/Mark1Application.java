b37a3b7a93bf23ef65d5a7ee9b4f22b555225a2f
Bruno Eilliar <engbrunoeilliar@gmail.com>
2023-10-20 19:22:16 -0300

    Spring project setup

---
+++ src/main/java/tech/ada/mark1/Mark1Application.java
@@ -0,0 +1,13 @@
+package tech.ada.mark1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark1Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/ada/mark1/Mark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.mark1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mark1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
