78c7bb99042ba650c5f2cb7830485149fb49166b
Lucas Vanzella <lucasvanzellap2@gmail.com>
2023-10-26 20:54:10 -0300

    models

---
+++ src/main/java/tech/ada/mark1/controller/AuthenticationController.java
@@ -0,0 +1,53 @@
+package tech.ada.mark1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.ada.mark1.security.TokenService;
+import tech.ada.mark1.model.AuthenticationDTO;
+import tech.ada.mark1.model.LoginResponseDTO;
+import tech.ada.mark1.model.RegisterDTO;
+import tech.ada.mark1.model.User;
+import tech.ada.mark1.repository.UserRepository;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthenticationController {
+
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private TokenService tokenService;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository, TokenService tokenService) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ResponseEntity.BodyBuilder> register(@RequestBody RegisterDTO data) {
+        if(this.userRepository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.username(), data.email(), encryptedPassword, data.role());
+
+        this.userRepository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/tech/ada/mark1/service/AuthorizationService.java
@@ -0,0 +1,22 @@
+package tech.ada.mark1.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tech.ada.mark1.repository.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
+}
