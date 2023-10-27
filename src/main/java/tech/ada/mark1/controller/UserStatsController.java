157f800b451c73d12e0be893efa136539e42314c
Renato <renato.jm@hotmail.com>
2023-10-27 15:40:30 -0300

    Improving endpoint Userstats, by passing all information in a hashmap

--- src/main/java/tech/ada/mark1/controller/UserStatsController.java
+++ src/main/java/tech/ada/mark1/controller/UserStatsController.java
@@ -2,11 +2,12 @@ package tech.ada.mark1.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import tech.ada.mark1.model.UserStats;
 import tech.ada.mark1.service.UserStatsService;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/userstats")
@@ -30,22 +31,14 @@ public class UserStatsController {
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<List<UserStats>> getHistoricScores(@PathVariable Long id) {
+    @GetMapping("/result")
+    public ResponseEntity<HashMap<String,Integer>> getScores(@RequestParam("email") String email) {
         try{
-            return new ResponseEntity<>(userStatsService.getHistoricScores(id),HttpStatus.OK);
+            return new ResponseEntity<>(userStatsService.getScores(email),HttpStatus.OK);
         } catch  (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/avg/{id}")
-    public ResponseEntity<Integer> getAvgScore(@PathVariable Long id) {
-        try{
-            return new ResponseEntity<>(userStatsService.getAverageScore(id), HttpStatus.OK);
-        } catch  (Exception e){
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 
 }
--- src/main/java/tech/ada/mark1/repository/UserRepository.java
+++ src/main/java/tech/ada/mark1/repository/UserRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import tech.ada.mark1.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByEmail(String email);
+
+    Optional<User> findUserByEmail(String email);
 
 }
