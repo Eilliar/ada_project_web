c4835a074c0139ba10ebf4cf7c6ad0fe65528b40
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-10-26 23:55:54 -0300

    adicionando end points

--- src/main/java/tech/ada/mark1/security/SecurityConfiguration.java
+++ src/main/java/tech/ada/mark1/security/SecurityConfiguration.java
@@ -3,8 +3,10 @@ package tech.ada.mark1.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,10 +16,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Autowired
@@ -37,6 +44,9 @@ public class SecurityConfiguration {
                         .requestMatchers(mvc.pattern("/h2-console/**")).permitAll()
                         .requestMatchers(mvc.pattern("/api/v1/auth/login")).permitAll()
                         .requestMatchers(mvc.pattern("/api/v1/auth/register")).permitAll()
+                        .requestMatchers(mvc.pattern("/api/v1/word")).permitAll()
+                        .requestMatchers(mvc.pattern("/api/v1/all")).permitAll()
+                        .requestMatchers(mvc.pattern("/api/v1/dictionary/**")).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
@@ -51,5 +61,17 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+                WebMvcConfigurer.super.addCorsMappings(registry);
+            }
+        };
     }
 }
